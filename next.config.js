405c8fcde3e1cebc6ee128c88f2d9dec60de72bc
unknown <smarttechiblog@gmail.com>
2026-04-04 19:35:24 -0700

    fix: update basePath to match new repository name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Resumebuilder',
+  basePath: '/makemyresume',
   images: {
     unoptimized: true,
   },
